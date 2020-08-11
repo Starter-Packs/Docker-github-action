349b206b21fb81d40189f8833ea17fe7a7cef3e0
Abhinav <abhinavtb@gmail.com>
2020-08-11 20:33:12 +0530

    web hook configured

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello")
+const axios = require('axios');
+
+axios.post('/test', {
+    firstName: 'Fred',
+    lastName: 'Flintstone'
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
